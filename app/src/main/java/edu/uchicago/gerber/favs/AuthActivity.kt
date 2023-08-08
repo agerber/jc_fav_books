a154af5a73675374f21381aece94e9f238951660
Adam Gerber <adam.gerber@yahoo.com>
2023-08-08 14:23:50 -0500

    code up AuthActivity

--- app/src/main/java/edu/uchicago/gerber/favs/AuthActivity.kt
+++ app/src/main/java/edu/uchicago/gerber/favs/AuthActivity.kt
@@ -1,46 +1,57 @@
 package edu.uchicago.gerber.favs
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.amazonaws.mobile.client.AWSMobileClient
+import com.amazonaws.mobile.client.Callback
+import com.amazonaws.mobile.client.SignInUIOptions
+import com.amazonaws.mobile.client.UserState
+import com.amazonaws.mobile.client.UserStateDetails
 
 
 class AuthActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        val TAG = AuthActivity::class.java.simpleName
 
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+        AWSMobileClient.getInstance()
+            .initialize(applicationContext, object : Callback<UserStateDetails> {
+                override fun onResult(userStateDetails: UserStateDetails) {
+                    Log.d(TAG, userStateDetails.userState.toString())
+                    when (userStateDetails.userState) {
+                        UserState.SIGNED_IN -> {
+                            val i = Intent(this@AuthActivity, MainActivity::class.java)
+                            startActivity(i)
+                        }
+
+                        UserState.SIGNED_OUT -> showSignIn()
+                        else -> {
+                            AWSMobileClient.getInstance().signOut()
+                            showSignIn()
+                        }
+                    }
                 }
-            }
 
+                override fun onError(e: Exception) {
+                    Log.e(TAG, e.toString())
+                }
+            })
+
+    }//end onCreate
+
+    private fun showSignIn() {
+        try {
+            AWSMobileClient.getInstance().showSignIn(
+                this,
+                SignInUIOptions.builder().nextActivity(MainActivity::class.java).build()
+            )
+        } catch (e: Exception) {
+            Log.e("AuthActivity", e.toString())
+        }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+}//end AuthActivity
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-
-        Greeting("Android")
-
-}
